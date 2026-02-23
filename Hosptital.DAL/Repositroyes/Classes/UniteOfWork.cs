3ee78534f232cfd56fe0eda175103e626e2f72ca
Gemy33 <mohamedgamalabdelhaliym@gmail.com>
2026-02-23 22:02:07 +0200

    Unite Of Work Design Pattern

---
+++ Hosptital.DAL/Repositroyes/Classes/UniteOfWork.cs
@@ -0,0 +1,41 @@
+﻿using Hosptital.DAL.Data.Contexts;
+using Hosptital.DAL.Entities.Base;
+using Hosptital.DAL.Repositroyes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hosptital.DAL.Repositroyes.Classes
+{
+    public class UniteOfWork<TEntity> : IUniteOfWork<TEntity> where TEntity : BaseEntity
+    {
+        private readonly HospitalDbContext hospitalDbContext;
+
+        public UniteOfWork(HospitalDbContext hospitalDbContext)
+        {
+            this.hospitalDbContext = hospitalDbContext;
+        }
+
+        Dictionary<Type, object> Repo = new Dictionary<Type, object>();
+        public IGenaricRepo<TEntity> GetGenaricRepo()
+        {
+            var genaricRepo = new GenaricRepo<TEntity>(hospitalDbContext);
+            if (Repo.ContainsKey(typeof(TEntity)))
+            {
+                return (IGenaricRepo<TEntity>)Repo[typeof(TEntity)];
+            }
+            else
+            {
+                Repo.Add(typeof(TEntity), genaricRepo);
+                return genaricRepo;
+            }
+        }
+
+        public int SaveChanges()
+        {
+            return hospitalDbContext.SaveChanges();
+        }
+    }
+}
---
+++ Hosptital.DAL/Repositroyes/Interfaces/IUniteOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Hosptital.DAL.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hosptital.DAL.Repositroyes.Interfaces
+{
+    public interface IUniteOfWork<TEntity> where TEntity : BaseEntity
+    {
+        IGenaricRepo<TEntity> GetGenaricRepo();
+        int SaveChanges();
+    }
+}
